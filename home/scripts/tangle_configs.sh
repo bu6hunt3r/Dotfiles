1f9fdf9f7945d084544d970bc49be6ee2dee8428
bu6hunt3r <bu6hunt3r@web.de>
2020-06-04 15:58:11 +0200

    Added tmux general options and keybindings into tmux.org

--- home/scripts/tangle_configs.sh
+++ home/scripts/tangle_configs.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 function tangle_files() {
-    DIR="/home/$USER/.homesick/repos/dotfiles/home/"
-    echo "current dir is: $DIR "
+    DIR="$(pwd)"
+    FILES=""
+
+    for i in `find $DIR -maxdepth 1 -iname "*.org" -type f`; do
+	FILES="$FILES \"$i\""
+    done
+
+    emacs -Q --batch \
+	  --eval \
+	  "(progn
+          (require 'org)
+          (require 'ob)
+          (require 'ob-tangle)
+          (mapc (lambda (file)
+	    (find-file (expand-file-name file \"$DIR\"))
+	    (org-babel-tangle)
+	    (kill-buffer)) '($FILES)))"
 }
 
 tangle_files
